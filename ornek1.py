590d702c60b0f041a128f27dd94303e3c7bea428
basakardal <ardalbasakaleyna@gmail.com>
2025-05-05 12:10:09 +0300

    Add files via upload

--- ornek1.py
+++ ornek1.py
@@ -1,8 +1,3 @@
-toplam=0
-s1=int(input("küçük olan sayıyı girin: "))
-s2=int(input("büyük olan sayıyı girin: "))
-
-for i in range(s1,s2):
-    if sayi % 2 == 0:
-        toplam=toplam+Sayi 
-print(s1,"ile",s2,"sayıları arasımdaki çift sayıların toplamı: ",toplam)
+isim="başak"
+for i in range(0,5):
+      print(isim)
